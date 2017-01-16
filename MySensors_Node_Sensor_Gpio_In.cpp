38af2f3f84205981c714806bdd3a745368c839c6
Dave Myers <brahmafear@gmail.com>
2017-01-16 08:06:23 -0500

    Minor

--- MySensors_Node_Sensor_Gpio_In.cpp
+++ MySensors_Node_Sensor_Gpio_In.cpp
@@ -42,7 +42,7 @@ MySensors_Node_Sensor* MySensors_Node_Sensor_Gpio_In::clone() const {
 } */
 
 void MySensors_Node_Sensor_Gpio_In::node_sensor_setup( ) {
-  DEBUG_MSG(F("[MySensors_Node_Sensor_Gpio_Input] Setup.\n"));
+  DEBUG_MSG(F("[MySensors_Node_Sensor_Gpio_In] Setup.\n"));
   pinMode( _pin, _input_type );
 }
 
@@ -52,14 +52,14 @@ void MySensors_Node_Sensor_Gpio_In::node_sensor_loop( ) {
   if ( millis() > next_read ) { // time to check
     bool state;
     if ( _pin == A6 || _pin == A7 ) { // On some Arduino boards, these pins don't work with digitalRead()
-      state = _active == (analogRead( _pin ) > 0);
+      state = (_active == (analogRead( _pin ) > 50));
     } else {
-      state = _active == digitalRead( _pin );
+      state = (_active == digitalRead( _pin ));
     }
 
     if ( state != last_state || next_read == 0 ) { // either something changed or first run
-      DEBUG_MSG(F("[MySensors_Node_Sensor_Gpio_Input] Sending state "));
-      DEBUG_MSG( state == _active ? F("active.\n") : F("inactive.\n"));
+      DEBUG_MSG(F("[MySensors_Node_Sensor_Gpio_In] Sending state "));
+      DEBUG_MSG( state ? F("active.\n") : F("inactive.\n"));
       send( MyMessage(get_sensor_id(), get_message_type()).set( state ? 1 : 0 ));
       last_state = state;
     }
